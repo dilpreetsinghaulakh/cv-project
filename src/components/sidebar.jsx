57bac4ae3008b9ad29f2d718715eeb96c6907321
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2023-12-28 13:55:54 +0530

    Add new fonts and update styles

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,9 @@
+import Info from "./sidebar/info";
+
+export default function Sidebar() {
+  return (
+    <section className="sidebar w-full xl:max-w-xl h-screen p-4">
+      <Info />
+    </section>
+  );
+}
---
+++ src/components/sidebar/info.jsx
@@ -0,0 +1,19 @@
+export default function Info() {
+  return (
+    <div className="border bg-white p-4 rounded-xl flex flex-col gap-2">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-xl text-gray-800">CV Maker</h1>
+        <a
+          href="https://github.com/dilpreetsinghaulakh/cv-project"
+          target="blank"
+          className="flex items-center justify-center gap-2 px-4 pl-5 rounded-lg py-2 focus:border text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-neutral-900 transition-colors"
+        >
+          <i className="ci-Arrow_Up_Right_MD scale-150"></i> Github
+        </a>
+      </div>
+      <p className="text-gray-700">
+        This is a simple CV Maker app made with React.js and TailwindCSS
+      </p>
+    </div>
+  );
+}
