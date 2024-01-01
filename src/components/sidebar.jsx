8dc368215cdd533c55a2794ff2dc7d762ceccee1
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-01 16:19:46 +0530

    Refactor sidebar and add gradient-blur effect with pattern body background

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,7 +3,11 @@ import Info from "./sidebar/info";
 
 export default function Sidebar() {
   return (
-    <section className="sidebar w-full xl:max-w-xl h-screen p-4 flex flex-col gap-2">
+    <section
+      className="w-full xl:max-w-xl h-screen p-4 flex flex-col gap-2"
+      id={"sidebar"}
+    >
+      <div className=" gradient-blur w-full xl:max-w-2xl h-screen p-4 fixed top-0 left-0 -z-10"></div>
       <Info />
       <DataBtn />
     </section>
