16239011c7eba70bceffcd043b951977364ca605
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-04 23:03:17 +0530

    Add font dependencies and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gruppo: ["Gruppo", "sans-serif"],
+        exo: ["Exo Variable", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
