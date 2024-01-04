1f7f2899cea3f46d8dc2f3b94154f2219e83ca00
dilpreetsinghaulakh <dilpreetsinghaulakh@icloud.com>
2024-01-04 23:03:33 +0530

    Add sampleData function to populate personal, education, and experience details

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,18 @@ import Info from "./sidebar/Info";
 import PersonalDetails from "./sidebar/PersonalDetails.jsx";
 import PropTypes from "prop-types";
 import WorkDetails from "./sidebar/WorkDetails.jsx";
+import sampleData from "./sampleData.js";
 
 export default function Sidebar(props) {
+  const getSets = () => {
+    let details = [];
+    for (let i = 0; i < Object.values(props.personalDetails).length; i++) {
+      details.push(Object.values(props.personalDetails)[i][1]);
+    }
+    return details;
+  };
+  const setPersonalDetails = getSets();
+
   return (
     <section
       className="w-full xl:max-w-xl xl:h-screen xl:overflow-scroll p-4 flex flex-col gap-2"
@@ -13,7 +23,15 @@ export default function Sidebar(props) {
     >
       <div className="hidden xl:block absolute gradient-blur w-full xl:max-w-2xl h-screen p-4 top-0 left-0 -z-10"></div>
       <Info />
-      <DataBtn />
+      <DataBtn
+        fillSampleData={() =>
+          sampleData(
+            setPersonalDetails,
+            props.educationalDetails[1],
+            props.workDetails[1]
+          )
+        }
+      />
       <PersonalDetails
         name={props.personalDetails.name}
         email={props.personalDetails.email}
---
+++ src/components/sampleData.js
@@ -0,0 +1,65 @@
+export default function sampleData(
+  setPersonalDetails,
+  setEducationDetails,
+  setExperienceDetails
+) {
+  const personalDetails = [
+    "John Doe",
+    "username@icloud.com",
+    "+0 000 000 0000",
+    "Amritsar, Punjab",
+  ];
+
+  for (let i = 0; i < personalDetails.length; i++) {
+    setPersonalDetails[i](personalDetails[i]);
+  }
+
+  setEducationDetails([
+    {
+      school: "Example University",
+      degree: "Bachelors of Technology",
+      startDate: "2023",
+      endDate: "2027",
+      location: "Amritsar, Punjab",
+    },
+    {
+      school: "Example School",
+      degree: "Senior Secondary",
+      startDate: "2022",
+      endDate: "2023",
+      location: "Amritsar, Punjab",
+    },
+    {
+      school: "Example School",
+      degree: "High School",
+      startDate: "2020",
+      endDate: "2021",
+      location: "Amritsar, Punjab",
+    },
+  ]);
+
+  setExperienceDetails([
+    {
+      company: "Example Company Inc.",
+      position: "Software Engineer",
+      startDate: "Late 2023",
+      endDate: "Present",
+      location: "Remote",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Non quam lacus suspendisse faucibus interdum posuere lorem. Nisl nisi scelerisque eu ultrices.
+      Eu lobortis elementum nibh tellus molestie nunc non. Sit amet nisl suscipit adipiscing bibendum est ultricies.
+      `,
+    },
+    {
+      company: "Example Company Inc.",
+      position: "Junior Software Engineer",
+      startDate: "2022",
+      endDate: "2023",
+      location: "Remote",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Non quam lacus suspendisse faucibus interdum posuere lorem. Nisl nisi scelerisque eu ultrices.
+      Eu lobortis elementum nibh tellus molestie nunc non. Sit amet nisl suscipit adipiscing bibendum est ultricies.
+      `,
+    },
+  ]);
+}
