c932414f29c4101fff41627949475900ae6a3cc8
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-06-11 16:53:39 +0530

    Added credit for the template

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -44,22 +44,24 @@ WorkDetails.propTypes = {
   details: PropTypes.object.isRequired,
 };
 
+const outputPaddingInRem = 5;
+
 export default function Output(props) {
   window.addEventListener("load", () => {
     if (window.innerWidth < 1280) {
       const scale =
         window.innerWidth /
         (553 +
-          4 * parseFloat(getComputedStyle(document.documentElement).fontSize));
+          outputPaddingInRem * parseFloat(getComputedStyle(document.documentElement).fontSize));
       document.getElementById("output").style.transform = `scale(${scale})`;
       document.getElementById("outputParent").style.height = `${780 * scale}px`;
-    }
-    else{
+    } else {
       const scale =
         window.innerHeight /
         (780 +
-          4 * parseFloat(getComputedStyle(document.documentElement).fontSize));
+          outputPaddingInRem * parseFloat(getComputedStyle(document.documentElement).fontSize));
       document.getElementById("output").style.transform = `scale(${scale})`;
+      document.getElementById("outputParent").style.height = `${780 * scale}px`;
     }
   });
 
@@ -68,7 +70,7 @@ export default function Output(props) {
       const scale =
         window.innerWidth /
         (553 +
-          4 * parseFloat(getComputedStyle(document.documentElement).fontSize));
+          outputPaddingInRem * parseFloat(getComputedStyle(document.documentElement).fontSize));
       document.getElementById("output").style.transform = `scale(${scale})`;
       document.getElementById("outputParent").style.height = `${780 * scale}px`;
     } else {
@@ -78,62 +80,84 @@ export default function Output(props) {
       const scale =
         window.innerHeight /
         (780 +
-          4 * parseFloat(getComputedStyle(document.documentElement).fontSize));
+          outputPaddingInRem * parseFloat(getComputedStyle(document.documentElement).fontSize));
       document.getElementById("output").style.transform = `scale(${scale})`;
+      document.getElementById("outputParent").style.height = `${780 * scale}px`;
     }
   });
 
   return (
-    <section
-      id="outputParent"
-      className="w-full xl:h-screen flex items-center justify-center my-4 xl:my-0"
-    >
-      <div
-        id="output"
-        className="a4-aspect px-4 py-10 h-[780px] bg-[url(./assets/output-background.jpg)] bg-cover flex gap-4 font-exo"
+    <div className="xl:max-h-screen flex flex-col justify-between gap-2 p-4 items-center w-full">
+      <section
+        id="outputParent"
+        className="w-full flex items-center justify-center"
       >
-        <div className="w-[40%] overflow-hidden">
-          <span className="flex justify-between">
-            <h2 className="text-xs">PERSONALS</h2>
-            <p className="text-xs">01</p>
-          </span>
-          <div className="bg-black h-[0.5px] w-full mt-1 mb-4"></div>
-          <div className="flex flex-col gap-2 ">
-            <Text text={props.personalDetails.email.toUpperCase()} />
-            <Text text={props.personalDetails.phone} />
-            <Text text={props.personalDetails.city.toUpperCase()} />
+        <div
+          id="output"
+          className="a4-aspect px-4 py-10 h-[780px] bg-[url(./assets/output-background.jpg)] bg-cover flex gap-4 font-exo"
+        >
+          <div className="w-[40%] overflow-hidden">
+            <span className="flex justify-between">
+              <h2 className="text-xs">PERSONALS</h2>
+              <p className="text-xs">01</p>
+            </span>
+            <div className="bg-black h-[0.5px] w-full mt-1 mb-4"></div>
+            <div className="flex flex-col gap-2 ">
+              <Text text={props.personalDetails.email.toUpperCase()} />
+              <Text text={props.personalDetails.phone} />
+              <Text text={props.personalDetails.city.toUpperCase()} />
+            </div>
+          </div>
+
+          <div className="">
+            <h1 className="font-gruppo font-bold text-4xl leading-8 ">
+              {props.personalDetails.name}
+            </h1>
+
+            <span className="flex justify-between mt-6">
+              <h2 className="text-xs">EDUCATION</h2>
+              <p className="text-xs">02</p>
+            </span>
+            <div className="bg-black h-[0.5px] w-full mt-1 mb-4"></div>
+            <div className="flex flex-col gap-3">
+              {props.educationalDetails.map((education, index) => {
+                return <EducationDetails details={education} key={index} />;
+              })}
+            </div>
+
+            <span className="flex justify-between mt-6">
+              <h2 className="text-xs">EXPERIENCE</h2>
+              <p className="text-xs">03</p>
+            </span>
+            <div className="bg-black h-[0.5px] w-full mt-1 mb-4"></div>
+            <div className="flex flex-col gap-3">
+              {props.workDetails.map((work, index) => {
+                return <WorkDetails details={work} key={index} />;
+              })}
+            </div>
           </div>
         </div>
+      </section>
 
-        <div className="">
-          <h1 className="font-gruppo font-bold text-4xl leading-8 ">
-            {props.personalDetails.name}
-          </h1>
-
-          <span className="flex justify-between mt-6">
-            <h2 className="text-xs">EDUCATION</h2>
-            <p className="text-xs">02</p>
-          </span>
-          <div className="bg-black h-[0.5px] w-full mt-1 mb-4"></div>
-          <div className="flex flex-col gap-3">
-            {props.educationalDetails.map((education, index) => {
-              return <EducationDetails details={education} key={index} />;
-            })}
-          </div>
-
-          <span className="flex justify-between mt-6">
-            <h2 className="text-xs">EXPERIENCE</h2>
-            <p className="text-xs">03</p>
-          </span>
-          <div className="bg-black h-[0.5px] w-full mt-1 mb-4"></div>
-          <div className="flex flex-col gap-3">
-            {props.workDetails.map((work, index) => {
-              return <WorkDetails details={work} key={index} />;
-            })}
-          </div>
-        </div>
-      </div>
-    </section>
+      <span className="bg-white/50 dark:bg-neutral-800/50 dark:text-white px-4 py-1 border border-white dark:border-neutral-700 backdrop-blur-sm shadow-lg shadow-black/5 w-fit rounded-full text-sm">
+        This{" "}
+        <a
+          href="https://www.figma.com/community/file/1038830156550702424"
+          className="text-blue-500 hover:underline"
+          target="blank"
+        >
+          template
+        </a>{" "}
+        is designed by{" "}
+        <a
+          href="https://www.behance.net/pavlova-e"
+          className="text-blue-500 hover:underline"
+          target="blank"
+        >
+          Ekaterina Pavlova
+        </a>
+      </span>
+    </div>
   );
 }
 
